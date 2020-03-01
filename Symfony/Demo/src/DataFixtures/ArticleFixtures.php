25e37a571f7e3a6de4ee718633081588ab63ea42
Yao Jean-Claude <jeandev84@yahoo.com>
2020-03-01 08:20:26 +0300

    Doctrine and RelationShip

--- Symfony/Demo/src/DataFixtures/ArticleFixtures.php
+++ Symfony/Demo/src/DataFixtures/ArticleFixtures.php
@@ -3,11 +3,16 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 
 /**
+ * $ bin/console doctrine:fixtures:load
+ *
  * Class ArticleFixtures
  * @package App\DataFixtures
 */
@@ -15,18 +20,65 @@ class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i < 10; $i++)
-        {
-            $article = new Article();
-            $article->setTitle("Titre de l'article numero $i")
-                    ->setContent("<p>Contenu de l'article numero $i</p>")
-                    ->setImage("http://placehold.it/350x150")
-                    ->setCreatedAt(new \DateTime());
+        $faker = Factory::create('fr_FR');
 
-            $manager->persist($article);
-            $manager->flush();
+        // Creer 3 categories fakees
+        for ($i=1; $i <= 3; $i++)
+        {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            // Creer entre 4 et 6 article
+            for($j = 1; $j <= mt_rand(4, 6); $j++)
+            {
+                $article = new Article();
+                $content = '<p>'. join($faker->paragraphs(5), '</p><p>') .'</p>';
+
+                $article->setTitle($faker->sentence())
+                    ->setContent($content)
+                    ->setImage($faker->imageUrl())
+                    ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                    ->setCategory($category);
+
+                $manager->persist($article);
+
+                # On donne des commentaires a l'article
+                for ($k =1; $k <= mt_rand(4, 10); $k++)
+                {
+                    $comment = new Comment();
+                    $content = '<p>'. join($faker->paragraphs(2), '</p><p>') .'</p>';
+
+                    /*
+                    # date actuel (maintenant)
+                    $now = new \DateTime();
+
+                    # difference entre la date de maintenant et la date de creation de l'article
+                    $interval = $now->diff($article->getCreatedAt());
+                    $days = $interval->days;
+                    $minimum = '-'. $days . ' days'; // -100 days
+
+                    $comment->setAuthor($faker->name)
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween($minimum));
+                    */
+                    $days = (new \DateTime())->diff($article->getCreatedAt())
+                                                 ->days;
+
+                    $comment->setAuthor($faker->name)
+                        ->setContent($content)
+                        ->setCreatedAt($faker->dateTimeBetween('-'. $days.' days'))
+                        ->setArticle($article);
+
+                    $manager->persist($comment);
+                }
+            }
         }
 
         $manager->flush();
     }
 }
+
+# bin/console doctrine:fixtures:load
